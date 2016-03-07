1dbbc986ecb9dd519cee35c253632aed1a713656
chrishanegraaf <chrishanegraaf@gmail.com>
2016-03-07 12:02:39 -0600

    adding more tests

--- Test.php
+++ Test.php
@@ -5,6 +5,12 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+	public function testTwo(){
+		$this->assertEquals(1,1);
+	}
+	public function testThree(){
+		$this->assertEquals(2,2);
+	}
 }
 
 ?>
